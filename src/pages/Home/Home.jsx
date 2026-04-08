8776f49f30ccc4b5bcdfe012e4aeacf152f3e78d
Thenula09 <thenulahansajawanniarachchi@gmail.com>
2026-04-09 00:42:31 +0530

    create home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 function Home() {
   return (
-    <section id="home" className="page-section">
-      <h1>FUROAN - Our Company Portfolio</h1>
-      <p>Welcome to the FUROAN website. This is our business portfolio.</p>
-      <p>
-        We specialize in <span className="highlight">web design, UI/UX design,</span> and <span className="highlight">React web development</span>.
-      </p>
-      <a className="button" href="#about">Learn More</a>
+    <section id="home" className="page-section home-section">
+      <div className="home-section__hero">
+        <div className="home-section__brand">
+          <div className="home-section__logo">
+            <span className="home-section__dot home-section__dot--green" />
+            <span className="home-section__dot home-section__dot--purple" />
+            <span className="home-section__dot home-section__dot--red" />
+          </div>
+          <h1 className="home-section__title">
+            <span className="home-section__letter home-section__letter--blue">F</span>
+            <span className="home-section__letter">U</span>
+            <span className="home-section__letter home-section__letter--white">O</span>
+            <span className="home-section__letter home-section__letter--red">R</span>
+            <span className="home-section__letter">A</span>
+            <span className="home-section__letter">N</span>
+          </h1>
+        </div>
+        <p className="home-section__subtitle">ALL software solution</p>
+        <p className="home-section__tagline">F U O R A N softer</p>
+      </div>
     </section>
   )
 }
